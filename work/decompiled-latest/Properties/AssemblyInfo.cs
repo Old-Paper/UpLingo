28a3904a2c8defcd31368565f44bc963964ccc50
PaperEX <146769828+Old-Paper@users.noreply.github.com>
2026-07-21 10:09:00 +0800

    Fix usage duration display clipping and release 1.10.2

--- work/decompiled-latest/Properties/AssemblyInfo.cs
+++ work/decompiled-latest/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.Versioning;
 using System.Security;
 using System.Security.Permissions;
 
-[assembly: AssemblyTitle("UpLingo v1.10.1")]
+[assembly: AssemblyTitle("UpLingo v1.10.2")]
 [assembly: AssemblyProduct("UpLingo")]
-[assembly: AssemblyFileVersion("1.10.1.0")]
-[assembly: AssemblyVersion("1.10.1.0")]
+[assembly: AssemblyFileVersion("1.10.2.0")]
+[assembly: AssemblyVersion("1.10.2.0")]
