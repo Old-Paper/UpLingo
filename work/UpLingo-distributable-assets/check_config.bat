1b25bbef3631d481546d80bd81fb3188a9a67146
PaperEX <146769828+Old-Paper@users.noreply.github.com>
2026-07-21 10:08:51 +0800

    Fix usage duration display clipping and release 1.10.2

--- work/UpLingo-distributable-assets/check_config.bat
+++ work/UpLingo-distributable-assets/check_config.bat
@@ -2,7 +2,7 @@
 chcp 65001 >nul
 cd /d "%~dp0"
 if exist fetch-test.log del fetch-test.log
-start /wait "" "%~dp0UpLingo-1.10.1.exe" --fetch-test
+start /wait "" "%~dp0UpLingo-1.10.2.exe" --fetch-test
 if exist fetch-test.log (
   type fetch-test.log
 ) else (
